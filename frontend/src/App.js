87e64531b556e3d069f6d498bcbbb6414306de70
Lauren Dove <elledoveinc@gmail.com>
2021-03-27 09:10:15 -0400

    creating a Product component in components folder. Will take props from HomeScreen.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,10 +11,11 @@ const App =() => {
      {/* py is padding on the y position. This gave padding between the main tag and the header */}
      <main className="py-3">
        <Container>
-       <h1>Welcome To Esther Prep</h1>
+       {/* <h1>Welcome To Esther Prep</h1> */}
+       <HomeScreen/>
        </Container>
      </main>
-     <HomeScreen/>
+     
      <Footer/>
     </>
   );
