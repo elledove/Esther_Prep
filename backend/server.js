13eb74729b3d1ba1eff77f6c9085fc4c6da94341
Lauren Dove <elledoveinc@gmail.com>
2021-04-20 12:53:58 -0400

    set up test get request in server.js Was successful port is working on 5000.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,19 @@
 //This will be our entry point for our backend server.
+const { response } = require('express');
 const express = require('express');
 const app = express();
+
+//This was a test to see if the get request is working properly and something was being sent to localhost 5000
+app.get('/',(request,response) => {
+    response.send('API is running Captian Picard....')
+})
+
+app.get('/api/products',(request,response) => {
+    response.send('API is running Captian Picard....')
+})
+
+
+
+
+
 app.listen(5000, console.log('Success Boss! Server is running on 5000'))
