94e6cb3c308fe18635dba086c371ea78fe749dac
Lauren Dove <elledoveinc@gmail.com>
2021-04-20 12:45:48 -0400

    created script inside of package-json. can run npm start and will run node backend/derver on port 5000.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,4 @@
 //This will be our entry point for our backend server.
+const express = require('express');
+const app = express();
+app.listen(5000, console.log('Success Boss! Server is running on 5000'))
