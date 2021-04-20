42990ed0fd933c1aba12ae50e09507ef398e9083
Lauren Dove <elledoveinc@gmail.com>
2021-04-20 13:07:12 -0400

    server.js has to get requests. One for all products, and one for each product based on the id number of that product.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-//This will be our entry point for our backend server.
-const { response } = require('express');
+//This file  will be our entry point for our backend server.
+// require('express') is allowing the express package ti be used in this file. 
+// const { response } = require('express');
 const express = require('express');
+//This line of code is allowing the file products to be used in this file.
+const products = require('./data/products')
 const app = express();
 
 //This was a test to see if the get request is working properly and something was being sent to localhost 5000
@@ -8,10 +11,15 @@ app.get('/',(request,response) => {
     response.send('API is running Captian Picard....')
 })
 
+//This is making a get request to the products and returning the information in a json format.
 app.get('/api/products',(request,response) => {
-    response.send('API is running Captian Picard....')
+    response.json(products)
 })
 
+app.get('/api/product/:id',(request,response) => {
+    const product = products.find(p => p._id === request.params.id)
+    response.json(product)
+})
 
 
 
